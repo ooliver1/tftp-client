aea2ca9f9dd470da2b2169c23185aff01b4d8e33
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-06-25 12:42:36 +0100

    refactor: use classes

--- main.cpp
+++ main.cpp
@@ -15,19 +15,45 @@ typedef struct {
     const char *mode;
 } Request;
 
-void kys(char const *s) {
+void kys(const char *s) {
     perror(s);
     exit(1);
 }
 
-int main(void) {
-    struct sockaddr_in server;
-    unsigned int sock;
-    socklen_t slen = sizeof(server);
-    char buf[BUFFER_LENGTH];
-    char message[BUFFER_LENGTH];
+class TFTP {
+    public:
+        TFTP(const char *server, unsigned short port);
+        ~TFTP();
+        void send(const Request &request);
+        void receive();
+    private:
+        unsigned int sock;
+        struct sockaddr_in server;
+};
 
-    Request request = {1, "/home/oliver/h.sh", "netascii"};
+TFTP::TFTP(const char *host, unsigned short port) {
+    if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        kys("socket");
+    }
+
+    // Clear `server` struct.
+    memset((char *)&server, 0, sizeof(server));
+    server.sin_family = AF_INET; // IPv4
+    server.sin_port = htons(port);
+
+    // Decode IP address dot notation into binary form.
+    if (inet_aton(host, &server.sin_addr) == 0) {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
+}
+
+TFTP::~TFTP() {
+    close(sock);
+}
+
+void TFTP::send(const Request &request) {
+    char message[BUFFER_LENGTH];
 
     // Construct message.
     sprintf(
@@ -37,26 +63,17 @@ int main(void) {
     unsigned int message_length =
         strlen(request.filename) + strlen(request.mode) + 4;
 
-    if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-        kys("socket");
-    }
-
-    // Clear `server` struct.
-    memset((char *)&server, 0, sizeof(server));
-    server.sin_family = AF_INET; // IPv4
-    server.sin_port = htons(PORT);
-
-    // Decode IP address dot notation into binary form.
-    if (inet_aton(SERVER, &server.sin_addr) == 0) {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
-    }
-
     if (sendto(
-            sock, message, message_length, 0, (struct sockaddr *)&server, slen
+            sock, message, message_length, 0, (struct sockaddr *)&server,
+            sizeof(server)
         ) == -1) {
         kys("sendto()");
     }
+}
+
+void TFTP::receive() {
+    char buf[BUFFER_LENGTH];
+    socklen_t slen = sizeof(server);
 
     memset(buf, '\0', BUFFER_LENGTH);
     if (recvfrom(
@@ -66,7 +83,14 @@ int main(void) {
     }
 
     puts(buf);
+}
 
-    close(sock);
+int main() {
+    TFTP tftp(SERVER, PORT);
+    Request request = {1, "/home/oliver/h.sh", "netascii"};
+
+    tftp.send(request);
+    tftp.receive();
+
     return 0;
 }
